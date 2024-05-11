6ce26158c3d096c213850e7370f94961bc510e04
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2024-05-11 16:17:59 +0200

    bump version

--- src/w3stringsx.py
+++ src/w3stringsx.py
@@ -15,7 +15,7 @@ from xml.etree import ElementTree
 # CONSTANTS AND ENUMS
 ###############################################################################################################################
 
-W3STRINGSX_VERSION = '1.2.0'
+W3STRINGSX_VERSION = '1.3.0'
 
 ALL_LANGS: list[str] = ['ar', 'br', 'cn', 'cz', 'de', 'en', 'es', 'esmx', 'fr', 'hu', 'it', 'jp', 'kr', 'pl', 'ru', 'tr', 'zh']
 ALL_LANGS_META: dict[str, str] = {
