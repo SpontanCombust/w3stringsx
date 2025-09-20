383aca1c5ac2fba337eb61c16247321feb75fbef
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2025-09-20 19:13:04 +0200

    adjusted tests to changes

--- tests/tests.py
+++ tests/tests.py
@@ -11,6 +11,16 @@ def file_hash(file_path: str) -> str:
         md5 = hashlib.md5(data)
         return md5.hexdigest()
     
+def listdir_recursive(dir_path: str) -> list[str]:
+    paths: list[str] = []
+    for root, dirs, files in os.walk(dir_path, topdown=False):
+        subdir = os.path.relpath(root, dir_path)
+        if subdir != '.':
+            dirs = [os.path.join(subdir, dir) for dir in dirs]
+            files = [os.path.join(subdir, file) for file in files]
+        paths.extend(dirs)
+        paths.extend(files)
+    return paths
 
 class Tests(unittest.TestCase):
     def test_decode_en(self):
@@ -62,10 +72,10 @@ class Tests(unittest.TestCase):
         self.run_case('parse_xml_custom_names')
 
     def test_decode_with_file_output(self):
-        self.run_case('decode_with_file_output', output_path='decoded.csv')
+        self.run_case('decode_with_file_output', output_path='decoded')
 
     def test_parse_xml_with_file_output(self):
-        self.run_case('parse_xml_with_file_output', output_path='parsed.csv')
+        self.run_case('parse_xml_with_file_output', output_path='parsed')
 
     def test_decode_path_with_spaces(self):
         self.run_case('decode path with spaces')
@@ -97,20 +107,20 @@ class Tests(unittest.TestCase):
 
     def run_case(self, case_name: str, extra_args: str = '', output_path: str | None = None, see_output: bool = False):
         root_dir = os.path.abspath(os.path.join(__file__, '../../'))
-        case_dir = f"{root_dir}/tests/{case_name}"
+        case_dir = os.path.join(root_dir, "tests", case_name)
 
-        input_dir = f"{case_dir}/input"
-        output_dir = f"{case_dir}/output"
-        output_preload_dir = f"{case_dir}/output_preload"
-        expected_dir = f"{case_dir}/expected"
+        input_dir = os.path.join(case_dir, "input")
+        output_dir = os.path.join(case_dir, "output")
+        output_preload_dir = os.path.join(case_dir, "output_preload")
+        expected_dir = os.path.join(case_dir, "expected")
 
-        input_path = f"{input_dir}/{os.listdir(input_dir)[0]}"
-        output_path = f"{output_dir}/{output_path}" if output_path is not None else output_dir
+        input_path = os.path.join(input_dir, os.listdir(input_dir)[0])
+        output_path = os.path.join(output_dir, output_path) if output_path is not None else output_dir
 
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
         if os.path.exists(output_preload_dir):
-            output_preload_path = f"{output_preload_dir}/{os.listdir(output_preload_dir)[0]}"
+            output_preload_path = os.path.join(output_preload_dir, os.listdir(output_preload_dir)[0])
             shutil.copy(output_preload_path, output_dir)
         
         cmd = f'python {root_dir}/src/w3stringsx.py "{input_path}" -o "{output_path}" {extra_args}'
@@ -123,34 +133,37 @@ class Tests(unittest.TestCase):
             self.assert_output(expected_dir, output_dir)
         finally:
             if not see_output:
-                for output in os.listdir(output_dir):
-                    output_path = os.path.join(output_dir, output)
-                    os.remove(output_path)
-
+                with os.scandir(output_dir) as it:
+                    for entry in it:
+                        if entry.is_dir():
+                            shutil.rmtree(entry.path)
+                        else:
+                            os.remove(entry.path)
 
     def assert_same_files(self, f1: str, f2: str):
-        self.assertTrue(os.path.exists(f1))
-        self.assertTrue(os.path.exists(f2))
+        self.assertTrue(os.path.exists(f1), f1 + " doesn't exist")
+        self.assertTrue(os.path.exists(f2), f2 + " doesn't exist")
 
         f1_hash = file_hash(f1)
         f2_hash = file_hash(f2)
         self.assertEqual(f1_hash, f2_hash)
 
     def assert_output(self, expected_dir: str, output_dir: str):
-        self.assertTrue(os.path.exists(output_dir))
+        self.assertTrue(os.path.exists(output_dir), output_dir + " doesn't exist")
 
-        expected_files = os.listdir(expected_dir)
+        expected_files = listdir_recursive(expected_dir)
         expected_files.sort()
 
-        output_files = os.listdir(output_dir)
+        output_files = listdir_recursive(output_dir)
         output_files.sort()
 
         self.assertEqual(expected_files, output_files)
 
         for i in range(len(expected_files)):
             e = os.path.join(expected_dir, expected_files[i])
-            o = os.path.join(output_dir, output_files[i]) 
-            self.assert_same_files(e, o)
+            o = os.path.join(output_dir, output_files[i])
+            if os.path.isfile(e) and os.path.isfile(o):
+                self.assert_same_files(e, o)
    
 
 if __name__ == '__main__':
