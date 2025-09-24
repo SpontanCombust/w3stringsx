1bb45f09082c22dcc5e05d1a86b70f0582482020
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2025-09-24 17:07:25 +0200

    added tasks for GUI project

---
+++ tasks/zip_gui.py
@@ -0,0 +1,32 @@
+import os
+import zipapp
+
+ROOT = os.path.realpath(os.path.dirname(os.path.dirname(__name__)))
+
+TARGET_DIR= os.path.join(ROOT, 'out')
+TARGET= os.path.join(TARGET_DIR, 'w3stringsx_gui.pyzw')
+WHITELIST = [
+    "w3stringsx/*.py",
+    "w3stringsx/lib/*.py",
+    "w3stringsx/gui/*.py",
+    "w3stringsx/gui/views/*.py"
+]
+BLACKLIST = [
+    '*/__pycache__',
+    '*/__pycache__/*',
+    '*.log'
+]
+
+
+if not os.path.isdir(TARGET_DIR):
+    os.mkdir(TARGET_DIR)
+    print('Created output directory ' + TARGET_DIR)
+
+zipapp.create_archive(
+    source=os.path.join(ROOT, 'src'),
+    main='w3stringsx.gui.application:application',
+    target=TARGET,
+    filter=(lambda p: any(p.match(pat) for pat in WHITELIST) and all(not p.match(pat) for pat in BLACKLIST))
+)
+
+print('w3stringsx GUI has been successfully packaged to ' + TARGET)
