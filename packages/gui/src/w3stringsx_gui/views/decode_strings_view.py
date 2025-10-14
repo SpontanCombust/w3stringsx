eb9ccf977b445f2c5aaef5dfce93ac2f2e508e17
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2025-10-14 14:10:11 +0200

    Refactor & error handling

--- packages/gui/src/w3stringsx_gui/views/decode_strings_view.py
+++ packages/gui/src/w3stringsx_gui/views/decode_strings_view.py
@@ -1,7 +1,10 @@
+from dataclasses import dataclass
 import os
+import traceback
 
 import flet as ft
 
+from w3stringsx_lib.logging import get_logger
 from w3stringsx_ioc import di, Injected
 from w3stringsx_svc import W3StringsManagerService
 from flet_reactive import Reactive, use_state
@@ -9,8 +12,12 @@ from w3stringsx_gui.routing import Router, Routes
 from w3stringsx_gui.components import LogsPanel
 
 
+_logger = get_logger()
+
+
+@dataclass
 class DecodeStringsViewProps:
-    w3strings_paths: list[str] = []
+    w3strings_paths: list[str]
 
 class DecodeStringsView(ft.View):
     def __init__(self, 
@@ -18,15 +25,16 @@ class DecodeStringsView(ft.View):
         w3strings_manager: Injected[W3StringsManagerService] = di.inject(W3StringsManagerService)
     ):
         self.__w3strings_manager = w3strings_manager.resolve()
-        self.__file_picker = ft.FilePicker(on_result=self.on_files_picked)
-        self.__output_dir_picker = ft.FilePicker(on_result=self.on_output_directory_picked)
-        self.__w3strings_paths = use_state(list[str]())
+
+        self.__w3strings_file_paths = use_state(list[str]())
         self.__output_dir_path = use_state('')
+        self.__w3strings_file_picker = ft.FilePicker(on_result=self.on_w3strings_files_picked)
+        self.__output_dir_picker = ft.FilePicker(on_result=self.on_output_dir_picked)
 
         if not isinstance(props, DecodeStringsViewProps):
-            props = DecodeStringsViewProps()
+            props = DecodeStringsViewProps(w3strings_paths=[])
         
-        self.__w3strings_paths.value.extend(props.w3strings_paths)
+        self.__w3strings_file_paths.value.extend(props.w3strings_paths)
         VISIBLE_W3STRINGS_PATHS_ROWS = 5
         
         super().__init__(
@@ -34,10 +42,13 @@ class DecodeStringsView(ft.View):
             controls=[
                 ft.AppBar(title=ft.Text("DECODING")),
                 ft.Column(
+                    expand=True,
                     controls=[
                         Reactive(
-                            [self.__w3strings_paths],
+                            [self.__w3strings_file_paths],
                             lambda: ft.ListView(
+                                auto_scroll=True,
+                                height=300,
                                 controls=[
                                     ft.DataTable(
                                         expand=True,
@@ -70,7 +81,7 @@ class DecodeStringsView(ft.View):
                                                         )),
                                                     ]
                                                 )
-                                                for path in self.__w3strings_paths.value
+                                                for path in self.__w3strings_file_paths.value
                                             ]
                                             +
                                             # add placeholders so all rows are visible
@@ -80,13 +91,11 @@ class DecodeStringsView(ft.View):
                                                         ft.DataCell(ft.Text(), placeholder=True),
                                                         ft.DataCell(ft.Text(), placeholder=True),
                                                     ]
-                                                ) for _ in range(VISIBLE_W3STRINGS_PATHS_ROWS - len(self.__w3strings_paths.value))
+                                                ) for _ in range(VISIBLE_W3STRINGS_PATHS_ROWS - len(self.__w3strings_file_paths.value))
                                             ]
                                         ],
                                     ),
                                 ],
-                                auto_scroll=True,
-                                height=300
                             ),
                         ),
                         ft.Row(
@@ -94,12 +103,12 @@ class DecodeStringsView(ft.View):
                                 ft.FilledButton(
                                     icon=ft.Icons.ATTACH_FILE,
                                     text="Add files...",
-                                    on_click=self.on_pick_file_button_click
+                                    on_click=self.on_pick_w3strings_file_button_click
                                 ),
                                 ft.FilledButton(
                                     icon=ft.Icons.CLEAR,
                                     text="Clear all",
-                                    on_click=self.on_clear_files_button_click
+                                    on_click=self.on_clear_w3strings_files_button_click
                                 )
                             ]
                         ),
@@ -117,27 +126,29 @@ class DecodeStringsView(ft.View):
                                 bgcolor=ft.Colors.PRIMARY_CONTAINER,
                                 color=ft.Colors.PRIMARY,
                                 border_color=ft.Colors.SECONDARY,
-                                on_click=self.on_pick_output_dir_button_click,
+                                on_click=self.on_output_dir_textfield_click,
                             ),
                         ),
+                        ft.Row(
+                            height=10
+                        ),
                         Reactive(
-                            [self.__w3strings_paths, self.__output_dir_path],
+                            [self.__w3strings_file_paths, self.__output_dir_path],
                             lambda: ft.Row(
+                                alignment=ft.MainAxisAlignment.CENTER,
                                 controls=[
                                     ft.FilledButton(
                                         icon=ft.Icons.LOCK_OPEN,
                                         text='DECODE',
                                         width=300,
                                         on_click=self.on_decode_button_click,
-                                        disabled=len(self.__w3strings_paths.value) == 0 
+                                        disabled=len(self.__w3strings_file_paths.value) == 0 
                                               or self.__output_dir_path.value == ''
                                     )
                                 ],
-                                alignment=ft.MainAxisAlignment.CENTER
                             ),
                         ),
                     ],
-                    expand=True
                 ),
                 LogsPanel(
                     height=180,
@@ -150,46 +161,57 @@ class DecodeStringsView(ft.View):
     def did_mount(self):
         super().did_mount()
         if (self.page):
-            self.page.overlay.append(self.__file_picker)
+            self.page.overlay.append(self.__w3strings_file_picker)
             self.page.overlay.append(self.__output_dir_picker)
             self.page.update()
 
     def will_unmount(self):
         super().will_unmount()
         if (self.page):
-            self.page.overlay.remove(self.__file_picker)
+            self.page.overlay.remove(self.__w3strings_file_picker)
             self.page.overlay.remove(self.__output_dir_picker)
 
-    def on_pick_file_button_click(self, ev: ft.ControlEvent):
-        self.__file_picker.pick_files(
+    def on_pick_w3strings_file_button_click(self, ev: ft.ControlEvent):
+        if len(self.__w3strings_file_paths.value) > 0:
+            last_w3strings_file_path = self.__w3strings_file_paths.value[-1]
+        else:
+            last_w3strings_file_path = ''
+        self.__w3strings_file_picker.pick_files(
             allow_multiple=True,
             allowed_extensions=['w3strings'],
-            file_type=ft.FilePickerFileType.CUSTOM
+            file_type=ft.FilePickerFileType.CUSTOM,
+            initial_directory=os.path.dirname(last_w3strings_file_path)
         )
 
-    def on_clear_files_button_click(self, ev: ft.ControlEvent):
-        self.__w3strings_paths.value = []
+    def on_clear_w3strings_files_button_click(self, ev: ft.ControlEvent):
+        self.__w3strings_file_paths.value = []
 
-    def on_files_picked(self, ev: ft.FilePickerResultEvent):
+    def on_w3strings_files_picked(self, ev: ft.FilePickerResultEvent):
         if ev.files is not None:
-            new_paths = self.__w3strings_paths.value.copy()
+            new_paths = self.__w3strings_file_paths.value.copy()
             for f in ev.files:
                 # filter duplicates, but preserve order
                 if f.path not in new_paths:
                     new_paths.append(f.path)
-            self.__w3strings_paths.value = new_paths
+            self.__w3strings_file_paths.value = new_paths
 
             # set default output path when picking the first file
             if self.__output_dir_path.value == '' and len(new_paths) == 1:
                 self.__output_dir_path.value = os.path.dirname(new_paths[0])
 
-    def on_pick_output_dir_button_click(self, ev: ft.ControlEvent):
-        self.__output_dir_picker.get_directory_path()
+    def on_output_dir_textfield_click(self, ev: ft.ControlEvent):
+        self.__output_dir_picker.get_directory_path(
+            initial_directory=self.__output_dir_path.value
+        )
 
-    def on_output_directory_picked(self, ev: ft.FilePickerResultEvent):
+    def on_output_dir_picked(self, ev: ft.FilePickerResultEvent):
         if ev.path is not None:
             self.__output_dir_path.value = ev.path
 
     def on_decode_button_click(self, ev: ft.ControlEvent):
-        for input_path in self.__w3strings_paths.value:
-            self.__w3strings_manager.decode_w3strings_to_csv(input_path, self.__output_dir_path.value)
+        for input_path in self.__w3strings_file_paths.value:
+            try:
+                self.__w3strings_manager.decode_w3strings_to_csv(input_path, self.__output_dir_path.value)
+            except Exception as ex:
+                _logger.error(ex)
+                _logger.error(traceback.format_exc())
